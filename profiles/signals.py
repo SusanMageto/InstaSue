7277fb3be5d3d3c685905e29251970752fd13760
SusanMageto <susan.mageto@student.moringaschool.com>
2022-06-08 03:22:14 +0300

    adds the signals.py file for saving the post

--- profiles/signals.py
+++ profiles/signals.py
@@ -3,6 +3,7 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from .models import Profile
 
+
 @receiver(post_save, sender=User)
 def post_save_create_profile(sender, instance, created, *args, **kwargs):
     if created:
