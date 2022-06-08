cb04b3656085d1fe08178454e0a6eb17e5d2855a
SusanMageto <susan.mageto@student.moringaschool.com>
2022-06-08 08:01:54 +0300

    fixes bugs in login

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -32,6 +32,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'instagram_clone.urls'
@@ -90,6 +91,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
     BASE_DIR / 'profiles' / 'static',
